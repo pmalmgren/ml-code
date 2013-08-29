384f64607afde13efbd5e4733ce12b99bb3b4491
Peter Malmgren <ptmalmyr@uncc.edu>
2013-08-29 19:43:23 -0400

    OCD

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -6,7 +6,7 @@ import numpy as np
 # Initialize data
 # dt is a data container containing rows/labels
 
-dt = np.dtype([('label',np.str_,1), ('data',np.int16, (16,))])
+dt = np.dtype([('label',np.str_, 1), ('data',np.int16, (16,))])
 
 testData = np.zeros((5000,1),dt)
 trainData = np.zeros((15000,1),dt)
@@ -16,17 +16,17 @@ counter = 0
 # Read in data
 
 with open('letter-recognition.data') as f:
-    content = f.readline()
-    content = content.split(',')
-    
-    label = content.pop(0)
-    data = np.int16(content)
-
-    if counter < 15000:
-        trainData.put(counter,[(label,data)])
-    else:
-        testData.put(counter-15000,[(label,data)])
-    
-    counter = counter + 1
+    for line in f:
+        content = line.split(',')
+        
+        label = content.pop(0)
+        data = np.int16(content)
+        
+        if counter < 15000:
+            trainData.put(counter,[(label,data)])
+        else:
+            testData.put(counter-15000,[(label,data)])
+        
+        counter = counter + 1
         
 print "Success!"
