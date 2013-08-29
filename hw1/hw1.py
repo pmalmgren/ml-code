d03531e0ce62c4f1c29682caf2a3011df9a7d467
Peter Malmgren <ptmalmyr@uncc.edu>
2013-08-29 18:59:13 -0400

    Added code to read in the data

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python2
 
+import scipy as sp
+import numpy as np
+
+# Initialize data
+
+trainingExamples = np.zeros((15000,16),dtype=np.int8)
+trainingLabels = []
+
+testExamples = np.zeros((5000,16),dtype=np.int16)
+testLabels = []
+
+counter = 0
+
 # Read in data
 
 with open('letter-recognition.data') as f:
-	content = f.readline();
-	
+    content = f.readline()
+    content = content.split(',')
+    
+    label = content.pop()
+    data = np.array(content,dtype=np.int16)    
+
+    if counter <= 14999:
+        trainingExamples[counter,:] = data
+        trainingLabels[counter] = label
+    else:
+        testExamples[counter-15000,:] = data
+        testLabels[counter] = label;
+        
