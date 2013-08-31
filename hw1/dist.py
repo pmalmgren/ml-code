928a739a7db2d79c575631b7c15e7a627367b695
Peter Malmgren <ptmalmyr@uncc.edu>
2013-08-31 11:15:27 -0400

    Figured out distance

--- hw1/dist.py
+++ hw1/dist.py
@@ -1,6 +1,14 @@
 # n number of samples, k number of neighbors
 # x training set, y new example
 
+import random
+from scipy.spatial.distance import cdist
+
 def knn(n,k,x,y):
     
+    # get our sample
+    sampleData = x[random.sample(range(15000),n)]
     
+    
+    
+    dm = cdist(XA, XB, lambda u, v: np.sqrt(((u-v)**2).sum()))
