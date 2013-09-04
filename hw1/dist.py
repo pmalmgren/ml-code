9aaf70db26eebe1d27be9f65a1cdcb7a4e0b1dc8
Peter Malmgren <ptmalmyr@uncc.edu>
2013-09-04 16:37:36 -0400

    added some nice looking comments

--- hw1/dist.py
+++ hw1/dist.py
@@ -1,9 +1,9 @@
-# n number of samples, k number of neighbors
-# x training set, y new example
-
 import random
 import numpy as np
 from scipy.spatial.distance import cdist
+
+# n number of samples, k number of neighbors
+# x training set, y new example
 
 def knn(n,k,x,y):
     # define custom data type to hold nearest neighbors
@@ -19,7 +19,7 @@ def knn(n,k,x,y):
     neighbors['data'] = dm.transpose()
     neighbors['labels'] = sampleData['labels']
     
-    # sort data, return k nearest neighbors
+    # sort data, return labels of k nearest neighbors
     knn = np.sort(neighbors,0)[1:k]['labels'].tolist()    
     
     maxCount = 0
@@ -30,6 +30,8 @@ def knn(n,k,x,y):
         if knn.count(item) > maxCount:
             maxCount = knn.count(item)
             classifiedLabel = item[0]
+    # end for item in knn
             
     return classifiedLabel
     
+# end knn()
--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python2
 
-import scipy as sp
 import numpy as np
+import dist
 
-# Initialize data
-# dt is a data container containing rows/labels
-
-dt = np.dtype([('labels',np.str_, 1), ('data',np.int16, (16,))])
-
-testData = np.zeros((5000,1),dt)
-trainData = np.zeros((15000,1),dt)
-
-counter = 0
-
-# Read in data
-
-with open('letter-recognition.data') as f:
-    for line in f:
-        content = line.split(',')
+def main():
+    # Initialize data
+    # dt is a data container containing rows/labels
+    
+    dt = np.dtype([('labels',np.str_, 1), ('data',np.int16, (16,))])
+    
+    testData = np.zeros((5000,1),dt)
+    trainData = np.zeros((15000,1),dt)
+    
+    counter = 0
+    
+    # Read in data
+    
+    with open('letter-recognition.data') as f:
+        for line in f:
+            content = line.split(',')
+            
+            label = content.pop(0)
+            data = np.int16(content)
+            
+            if counter < 15000:
+                trainData.put(counter,[(label,data)])
+            else:
+                testData.put(counter-15000,[(label,data)])
+            
+            counter = counter + 1
+         # end for line in f
+    # end with open()
+    
+    
         
-        label = content.pop(0)
-        data = np.int16(content)
-        
-        if counter < 15000:
-            trainData.put(counter,[(label,data)])
-        else:
-            testData.put(counter-15000,[(label,data)])
-        
-        counter = counter + 1
-        
-print "Success!"
+
+if __name__ == "__main__":
+    main()
