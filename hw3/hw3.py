052e2843391a2826b0f451c1e1df77057c9ab4c9
Peter Malmgren <ptmalmyr@uncc.edu>
2013-10-24 13:59:04 -0400

    added some hw3 stuff (data and a skeleton file)

---
+++ hw3/hw3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+main.py
+
+reads in data, trains classifier using AdaBoost, then outputs a number of 
+relevant statistics.
+
+"""
+
+import numpy as np
+
+def main():
+    
+    
+if __name__ == '__main__':
+    main()
